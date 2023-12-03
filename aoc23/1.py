75b68dfcb647278ba2d229e943081aaef22d44e8
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2023-12-03 11:12:16 -0500

    solve 1.1

--- aoc23/1.py
+++ aoc23/1.py
@@ -1,25 +1,48 @@
 # On each line, the calibration value can be found by combining the first digit
 # and the last digit (in that order) to form a single two-digit number.
 
-calibration_list = ['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet']
-
-calibrations = []
-for s in calibration_list:
-    calibration = []
-    for c in s:
-        try:
-            d = int(c)
-            calibration.append(c)
-        except ValueError:
-            pass
-    calibrations.append(calibration)
-print(calibrations)
-
-result = 0
-for c in calibrations:
-    first_and_last = int(c[0] + c[-1])
-    result = result + first_and_last
+# input = ['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet']
+input = open('./input/1.txt', 'r')
 
 
-assert result == 142, 'incorrect result'
-print(result)
+def day1part1():
+    calibrations = []
+    for string in input:
+        calibration = []
+        for char in string:
+            try:
+                int(char)
+                calibration.append(char)
+            except ValueError:
+                pass
+        calibrations.append(calibration)
+
+    sum = 0
+    for c in calibrations:
+        first_and_last = int(c[0] + c[-1])
+        sum += first_and_last
+
+    return sum
+
+
+print(day1part1())
+
+
+def day1part2():
+    calibrations = []
+    for string in input:
+        calibration = []
+        for char in string:
+            try:
+                int(char)
+                calibration.append(char)
+            except ValueError:
+                pass
+        calibrations.append(calibration)
+
+    sum = 0
+    for c in calibrations:
+        first_and_last = int(c[0] + c[-1])
+        sum += first_and_last
+
+    return sum
