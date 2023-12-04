e2184a9bd438a403e0cd41c207dbf65477c2731d
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2023-12-03 22:16:32 -0500

    day2 part2

--- aoc23/2.py
+++ aoc23/2.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 input = open('./input/2.txt', 'r')
 input1 = [
     'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green',  # True
@@ -13,6 +15,8 @@ RED, GREEN, BLUE = 12, 13, 14
 # The Elf would first like to know which games would have been possible if
 # the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
 # What is the sum of the IDs of those games?
+
+
 def day2part1():
     game_number = 1
     sum = 0
@@ -51,44 +55,35 @@ def day2part1():
     return sum
 
 
-print(day2part1())
+# print(day2part1())
 
-# For each game, find the minimum set of cubes that must have been present.
+# For each game, find the MINIMUM SET OF CUBES that must have been present.
 # What is the sum of the power of these sets?
+# eg: Game 2 could have been played with a minimum of 1 red, 3 green, and 4 blue cubes.
+
+
 def day2part2():
-    game_number = 1
     sum = 0
 
     for row in input:
         draws = row.split(':')[1]
         sets = draws.split(';')
-        possible = True
-        # print(sets)
+        max_d = {'red': 0, 'green': 0, 'blue': 0}
         for s in sets:
-            # print(s)
             colors = s.split(',')
-            # print(colors)
             for color in colors:
-                # print(color)
                 if 'red' in color:
                     v = color.replace('red', '').strip()
-                    if int(v) > RED:
-                        possible = False
-                        break
+                    max_d['red'] = max(max_d['red'], int(v))
                 elif 'green' in color:
                     v = color.replace('green', '').strip()
-                    if int(v) > GREEN:
-                        possible = False
-                        break
+                    max_d['green'] = max(max_d['green'], int(v))
                 elif 'blue' in color:
                     v = color.replace('blue', '').strip()
-                    if int(v) > BLUE:
-                        possible = False
-                        break
-        if possible:
-            sum = sum + game_number
+                    max_d['blue'] = max(max_d['blue'], int(v))
 
-        game_number += 1
+        power = reduce(lambda x, y: x * y, max_d.values())
+        sum += power
 
     return sum
 
