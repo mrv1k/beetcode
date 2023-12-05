839f27119d02f413a292b5b2863d10374b971ec7
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2023-12-05 10:28:13 -0500

    d3p1 solve example case

--- aoc23/3.py
+++ aoc23/3.py
@@ -1,13 +1,14 @@
-input = open('./input/3.txt', 'r')
-input1 = [
-    '467..114..',
-    '...*......',
+import re
+# input = open('./input/3.txt', 'r')
+input1 = [  # #      ∅∅∅∅∅
+    '467..114..',  # .114. -> {114} is not adjacent to any symbols
+    '...*......',  # .....
     '..35..633.',
     '......#...',
     '617*......',
-    '.....+.58.',
-    '..592.....',
-    '......755.',
+    '.....+.58.',  # ....
+    '..592.....',  # .58. -> {58} is not adjacent to any symbols
+    '......755.',  # ....
     '...$.*....',
     '.664.598..',
 ]
@@ -15,12 +16,55 @@ input1 = [
 # The engine schematic (your puzzle input) consists of a visual representation
 # of the engine.
 # (Periods (.) do not count as a symbol.)
-# add up all the part numbers
-# any number adjacent to a symbol, even diagonally, is a "part number" and
-# should be included in your sum
 
-NULL = '.'
+# ANY NUMBER ADJACENT TO A SYMBOL, even DIAGONALLY, is a "part number" and
+# should be included in your sum
+# add up all the part numbers
+
+NOT_EVEN_SYMBOL = '.'
+ROW_LENGTH = len(input1[0])
+RE_DIGIT = r'\d+'
+RE_SYMBOL = r'[^\w\s.]'
 
 
 def day3part1():
-    print('mekmek')
+    dummy_row = NOT_EVEN_SYMBOL * ROW_LENGTH
+
+    sum = 0
+    for i, row in enumerate(input1):
+        first_row = i == 0
+        last_row = i == len(input1) - 1
+        row_above = dummy_row if first_row else input1[i - 1]
+        row_above = 'x' + row_above + 'x'
+        row = 'x' + row + 'x'
+        row_below = dummy_row if last_row else input1[i + 1]
+        row_below = 'x' + row_below + 'x'
+
+        # print(row_above)
+        # print(row)
+        # print(row_below)
+        # print()
+        for digit in re.finditer(RE_DIGIT, row):
+            start = digit.start() - 1
+            end = digit.end() + 1
+
+            around_above = row_above[start:end]
+            around_number = row[start:end]
+            around_below = row_below[start:end]
+            # print(around_above, symbol_above)
+            # print(around_number, symbol_next_to)
+            # print(around_below, symbol_below)
+            # print()
+
+            symbol_above = re.findall(RE_SYMBOL, around_above)
+            symbol_next_to = re.findall(RE_SYMBOL, around_number)
+            symbol_below = re.findall(RE_SYMBOL, around_below)
+
+            around = symbol_above + symbol_next_to + symbol_below
+            if around:
+                sum += int(digit.group())
+
+    return sum
+
+
+print(day3part1())
