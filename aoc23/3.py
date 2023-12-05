a51dec30371f457c1d387092ef0e6a47bd90da85
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2023-12-05 09:42:16 -0500

    d3 progress

---
+++ aoc23/3.py
@@ -0,0 +1,26 @@
+input = open('./input/3.txt', 'r')
+input1 = [
+    '467..114..',
+    '...*......',
+    '..35..633.',
+    '......#...',
+    '617*......',
+    '.....+.58.',
+    '..592.....',
+    '......755.',
+    '...$.*....',
+    '.664.598..',
+]
+
+# The engine schematic (your puzzle input) consists of a visual representation
+# of the engine.
+# (Periods (.) do not count as a symbol.)
+# add up all the part numbers
+# any number adjacent to a symbol, even diagonally, is a "part number" and
+# should be included in your sum
+
+NULL = '.'
+
+
+def day3part1():
+    print('mekmek')
