0b9cb20fb6e42cbea91bac8d594b685b08b3ee04
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2023-12-12 23:37:07 -0500

    solve 6p1

---
+++ aoc23/6.py
@@ -0,0 +1,34 @@
+from functools import reduce
+
+# go farther in each race
+# Holding down the button charges the boat
+# releasing the button allows the boat to move
+
+
+def day6part1():
+    with open('./input/6.txt', 'r') as file:
+        times = map(int, file.readline().split()[1:])
+        distances = map(int, file.readline().split()[1:])
+        distances = list(distances)
+        record_pairs = []
+        for i, time in enumerate(times):
+            record_pairs.append((time, distances[i]))
+
+        print(record_pairs)
+        ways_to_beat = []
+
+        for record in record_pairs:
+            the_meat = 0
+            hodl = 0
+            while hodl < record[0] - 1:
+                hodl += 1
+                travel = hodl * (record[0] - hodl)
+                beat = travel > record[1]
+                if beat:
+                    the_meat += 1
+            ways_to_beat.append(the_meat)
+        print(ways_to_beat)
+        return reduce(lambda x, y: x * y, ways_to_beat)
+
+
+print(day6part1())
